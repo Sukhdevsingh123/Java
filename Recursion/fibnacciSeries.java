84dd4c467fa9d64fd02bdc96cdd9e615781d0bae
Sukhdevsingh321 <9664sukhdevsingh@gmail.com>
2023-11-17 00:11:38 -0800

    fibnacci

--- Recursion/fibnacciSeries.java
+++ Recursion/fibnacciSeries.java
@@ -1,6 +1,14 @@
 public class fibnacciSeries{
+    public static int  fibnoci(int n){
+        if(n==0||n==1){
+          return n;
+}
+
+        return fibnoci(n-1)+fibnoci(n-2);    
+    }
 public static void main(String args[]){
-    System.out.println("fabnicci seroise");
+  int n=6;
+  System.out.println(fibnoci(n));
 }
 
 }
