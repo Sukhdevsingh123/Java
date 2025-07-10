9a2182925490f188f817805dd4267a257dc9b755
Sukhdevsingh123 <sukhdev72361234@gmail.com>
2025-07-10 15:34:55 +0530

    changes

--- DSA/day1.java
+++ DSA/day1.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
-public class day1{
+public class day1 {
 
-public static int sumOfOdd(int start,int end){
-    int sum=0;
-    for(int i=start;i<=end;i++){
+    public static int sumOfOdd(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
 
-        if(i%2!=0){
-            sum+=i;
+            if (i % 2 != 0) {
+                sum += i;
+            }
         }
+        return sum;
     }
-    return sum;
-}
 
-    public static void main(String args []){
+    public static void main(String args[]) {
 
-        int result=sumOfOdd(1,100);
-        System.out.println("Sum of 1 to 100 odd no is: " +result);//Sum of 1 to 100 odd no is: 2500
+        int result = sumOfOdd(1, 100);
+        System.out.println("Sum of 1 to 100 odd no is: " + result);// Sum of 1 to 100 odd no is: 2500
+        System.out.println("hello");
     }
 }
