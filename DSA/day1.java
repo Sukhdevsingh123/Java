6de5149147235f67416d1939e5d79fc05127d72d
Sukhdevsingh123 <sukhdev72361234@gmail.com>
2025-07-10 15:20:36 +0530

    Sum of Odd

---
+++ DSA/day1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class day1{
+
+public static int sumOfOdd(int start,int end){
+    int sum=0;
+    for(int i=start;i<=end;i++){
+
+        if(i%2!=0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
+
+    public static void main(String args []){
+
+        int result=sumOfOdd(1,100);
+        System.out.println("Sum of 1 to 100 odd no is: " +result);//Sum of 1 to 100 odd no is: 2500
+    }
+}
